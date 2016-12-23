cf4777627fb63b0a17d57c581a6874cf4ff09196
antonioudakis <t.antonioudakis@gmail.com>
2016-12-23 14:00:10 +0200

    added workspace

---
+++ workspace/rest/src/main/java/org/ntua/tant/rest/client/restclient.java
@@ -0,0 +1,205 @@
+package org.ntua.tant.rest.client;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.glassfish.jersey.internal.util.Base64;
+
+public class restclient {
+	
+	private String method;
+	private String fileName;
+    private String userName;
+    private String password;
+    private String url;
+    private static final String Coding = "UTF-8";
+	//private static final String URLdefault = "http://147.102.213.13:8888/rest/webapi/messages";
+	private static final String ErrorInParameters = "Name: restclient\n\nDescription: Calls a ReST Service\n\nThe following parameters are needed:\n\n-m   ReST Method to call. The implemented methods are GET, POST, PUT and DELETE.\n-f   Input XML file for POST and PUT methods. For GET method output file is optional.\n     When a file is not defined in GET method the responded XML file is printed on the screen.\n     For Delete method file is not needed.\n-URL Service URL\n\nThe following parameters are optional and they are needed only if ReST service requires Basic Authentication:\n\n-u Username\n-p Password\n";
+	
+
+	private  void parseParameters(String[] args){
+		for (int i=0;i < args.length;i++){
+			if ((args[i].equals("-m")) && (i<args.length-1)){
+				method=args[++i].toUpperCase();
+			} else if ((args[i].equals("-f")) && (i<args.length-1)){
+				fileName=args[++i];
+			} else if ((args[i].equals("-u")) && (i<args.length-1)){
+				userName=args[++i];
+			} else if ((args[i].equals("-p")) && (i<args.length-1)){
+				password=args[++i];
+			} else if ((args[i].equals("-URL")) && (i<args.length-1)){
+				url=args[++i];
+			} else if (args[i].equals("-h")) {
+				System.out.println(ErrorInParameters);
+				System.exit(0);
+		    }
+		}
+		if ((method == null)||(url == null)||(((method.equals("POST"))||(method.equals("PUT")))&&(fileName == null))) {
+			System.out.println(ErrorInParameters);
+			System.exit(-1);
+		}
+	}
+	
+	private void getResponseFromGETRequest(String url, String fileName, String username, String password) {
+	    try {
+	    	URL object = new URL(url);
+	        HttpURLConnection connection = (HttpURLConnection) object.openConnection();
+	        connection.setReadTimeout(60 * 1000);
+	        connection.setConnectTimeout(60 * 1000);
+	        connection.setDoInput(true);
+	        connection.setRequestMethod("GET");
+	        connection.setRequestProperty("Content-Type", "application/XML");
+	        if ((username!=null)&&(password!=null)) {
+	        	String authorization=username+":"+password;
+	            String encodedAuth="Basic "+Base64.encodeAsString(authorization);
+	            connection.setRequestProperty("Authorization", encodedAuth);
+	        }
+	        int responseCode = connection.getResponseCode();
+	        if (responseCode != 200) {
+	        	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+			    System.exit(-1);
+	        } else {
+	            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+	            String line;
+            	if (fileName != null) {
+            		PrintWriter writer = new PrintWriter(fileName, Coding);
+            		while ((line = rd.readLine()) != null) {
+    	            	writer.println(line);
+    	            }
+            		writer.close();
+            		System.out.println("Response Code: 200 OK. XML response has been written in a file called "+fileName);
+            	} else {
+            		while ((line = rd.readLine()) != null) {
+    	            	System.out.println(line);;
+            		}
+	            }
+	            rd.close();
+            }
+	    } catch (Exception e) {
+            e.getMessage();
+        }
+	}
+	
+	public void getResponseFromPOSTRequest(String url, String fileName, String username, String password) {
+	    String fileData = new String();
+	    try {
+	    	StringBuilder postData = new StringBuilder();
+	        FileReader fr = new FileReader(fileName);
+	    	BufferedReader br = new BufferedReader(fr);
+    		String sCurrentLine;
+	    	while ((sCurrentLine = br.readLine()) != null) {
+	    		fileData+=sCurrentLine; 
+	    	}
+	    	br.close();
+	    	postData.append(fileData);
+	    	byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+	        URL object = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) object.openConnection();
+            connection.setReadTimeout(60 * 1000);
+            connection.setConnectTimeout(60 * 1000);
+            connection.setDoInput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/XML");
+            if ((username!=null)&&(password!=null)) {
+	        	String authorization=username+":"+password;
+	            String encodedAuth="Basic "+Base64.encodeAsString(authorization);
+	            connection.setRequestProperty("Authorization", encodedAuth);
+	        }
+            connection.setDoOutput(true);
+            connection.getOutputStream().write(postDataBytes);
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+	        	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+			    System.exit(-1);
+	        } else {
+	          	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+	        }            
+	    } catch (Exception e){
+	    	System.out.println(e.getMessage());
+	    }
+	}
+	
+	public void getResponseFromPUTRequest(String url, String fileName, String username, String password) {
+	    String fileData = new String();
+	    try {
+	    	StringBuilder postData = new StringBuilder();
+	        FileReader fr = new FileReader(fileName);
+	    	BufferedReader br = new BufferedReader(fr);
+    		String sCurrentLine;
+	    	while ((sCurrentLine = br.readLine()) != null) {
+	    		fileData+=sCurrentLine; 
+	    	}
+	    	br.close();
+	    	postData.append(fileData);
+	    	byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+	        URL object = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) object.openConnection();
+            connection.setReadTimeout(60 * 1000);
+            connection.setConnectTimeout(60 * 1000);
+            connection.setDoInput(true);
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("Content-Type", "application/XML");
+            if ((username!=null)&&(password!=null)) {
+	        	String authorization=username+":"+password;
+	            String encodedAuth="Basic "+Base64.encodeAsString(authorization);
+	            connection.setRequestProperty("Authorization", encodedAuth);
+	        }
+            connection.setDoOutput(true);
+            connection.getOutputStream().write(postDataBytes);
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+	        	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+			    System.exit(-1);
+	        } else {
+	          	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+	        }            
+	    } catch (Exception e){
+	    	System.out.println(e.getMessage());
+	    }
+	}
+	
+	public void getResponseFromDELETERequest(String url, String username, String password) {
+	    try {
+	        URL object = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) object.openConnection();
+            connection.setReadTimeout(60 * 1000);
+            connection.setConnectTimeout(60 * 1000);
+            connection.setDoInput(true);
+            connection.setRequestMethod("DELETE");
+            connection.setRequestProperty("Content-Type", "application/XML");
+            if ((username!=null)&&(password!=null)) {
+	        	String authorization=username+":"+password;
+	            String encodedAuth="Basic "+Base64.encodeAsString(authorization);
+	            connection.setRequestProperty("Authorization", encodedAuth);
+	        }
+            int responseCode = connection.getResponseCode();
+            if (responseCode != 200) {
+	        	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+			    System.exit(-1);
+	        } else {
+	          	System.out.println("Response Code: "+responseCode+" "+connection.getResponseMessage());
+	        }            
+	    } catch (Exception e){
+	    	System.out.println(e.getMessage());
+	    }
+	}
+	
+	public static void main(String[] args) {
+		restclient ReST = new restclient();
+		ReST.parseParameters(args);
+        if (ReST.method.equals("GET")){
+        	ReST.getResponseFromGETRequest(ReST.url, ReST.fileName, ReST.userName, ReST.password);	
+        } else if(ReST.method.equals("POST")) {
+        	ReST.getResponseFromPOSTRequest(ReST.url, ReST.fileName, ReST.userName, ReST.password);	
+        } else if(ReST.method.equals("PUT")) {
+        	ReST.getResponseFromPUTRequest(ReST.url, ReST.fileName, ReST.userName, ReST.password);	
+        } else if(ReST.method.equals("DELETE")) {
+        	ReST.getResponseFromDELETERequest(ReST.url, ReST.userName, ReST.password);	
+        } else {
+        	System.out.println(ErrorInParameters);
+        }
+	}
+}
---
+++ workspace/rest/src/main/java/org/ntua/tant/rest/xml/ReadXMLFile.java
@@ -0,0 +1,104 @@
+package org.ntua.tant.rest.xml;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXNotSupportedException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class ReadXMLFile {
+	public static void main(String argv[]) {
+
+	    try {
+
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser saxParser = factory.newSAXParser();
+
+		DefaultHandler handler = new DefaultHandler() {
+
+		boolean bfname = false;
+		boolean blname = false;
+		boolean bnname = false;
+		boolean bsalary = false;
+
+		public void startElement(String uri, String localName,String qName,
+	                Attributes attributes) throws SAXNotSupportedException {
+
+			System.out.println("Start Element :" + qName);
+
+			if (qName.equalsIgnoreCase("FIRSTNAME")) {
+				bfname = true;
+			}
+
+			if (qName.equalsIgnoreCase("LASTNAME")) {
+				blname = true;
+			}
+
+			if (qName.equalsIgnoreCase("NICKNAME")) {
+				bnname = true;
+			}
+
+			if (qName.equalsIgnoreCase("SALARY")) {
+				bsalary = true;
+			}
+
+		}
+
+		public void endElement(String uri, String localName,
+			String qName) throws SAXException {
+
+			System.out.println("End Element :" + qName);
+
+		}
+
+		public void characters(char ch[], int start, int length) throws SAXException {
+
+			if (bfname) {
+				System.out.println("First Name : " + new String(ch, start, length));
+				bfname = false;
+			}
+
+			if (blname) {
+				System.out.println("Last Name : " + new String(ch, start, length));
+				blname = false;
+			}
+
+			if (bnname) {
+				System.out.println("Nick Name : " + new String(ch, start, length));
+				bnname = false;
+			}
+
+			if (bsalary) {
+				System.out.println("Salary : " + new String(ch, start, length));
+				bsalary = false;
+			}
+
+		}
+
+	     };
+	       File file = new File("c:\\Users\\thodoris\\fileNew.xml");
+	       InputStream inputStream = new FileInputStream(file);
+ 	       Reader reader = new InputStreamReader(inputStream,"UTF-8");
+
+ 	       InputSource is = new InputSource(reader);
+ 	       is.setEncoding("UTF-8");
+ 	       saxParser.parse(is, handler);
+
+
+	     } catch (Exception e) {
+	       e.printStackTrace();
+	     }
+
+	   }
+
+}
